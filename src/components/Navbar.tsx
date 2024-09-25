28c9bd603c935ebe44313e23a09dbc75105fe8a9
Nikki Mae Evers <37475750+nikkimae@users.noreply.github.com>
2024-09-25 13:18:51 +0200

    working on clicked styles, not working yet

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,49 +3,52 @@ import Link from 'next/link';
 const Navbar = () => {
   return (
     <div className="navbar h-16 bg-base-100 justify-between px-6">
+      {/* NAME / LOGO */}
+
+      {/* PAGES */}
       <ul className="menu menu-horizontal space-x-4">
-        <li>
-        <Link href="/" className="hover:bg-primary hover:text-white h-16 flex rounded-none hover:text-lg transition-all duration-200">Home</Link>
+        <li className="menu-li">
+        <Link href="/" className="menu-link">Home</Link>
         </li>
-        <li>
-        <Link href="/" className="hover:bg-primary hover:text-white">Home</Link>
-        </li>
-        <li>
-        <Link href="/" className="hover:bg-gradient-to-r hover:from-primary hover:to-secondary hover:text-white">Home</Link>
-        </li>
-        <li>
-        <Link href="/" className="hover:scale-105 transition-transform duration-200 hover:bg-primary hover:text-white">Home</Link>
-        </li>
-        <li>
-        <Link href="/" className="hover:shadow-lg hover:bg-primary hover:text-white transition-shadow">Home</Link>
-        </li>
-        <li>
-        <Link href="/" className="hover:bg-gradient-to-r hover:from-primary hover:to-secondary hover:text-white hover:scale-105 transition-transform duration-200 hover:shadow-lg">Home</Link>
-        </li>
-        <li>
-        <Link href="/" className="hover:shadow-lg hover:scale-105 transition-transform duration-200 hover:bg-primary hover:text-white">Home</Link>
-        </li>
-
-
-        {/* Underline options */}
-        <li>
-        <Link href="/" className="border-none hover:bg-transparent hover:font-bold hover:text-primary after:absolute after:w-full after:h-0.5 after:bg-primary after:left-0 after:bottom-0 after:scale-x-0 hover:after:scale-x-100 after:transition-transform">Home</Link>
-        </li>
-        <li>
-        <Link href="/" className="hover:bg-transparent hover:font-bold  after:absolute after:w-full after:h-0.5 after:bg-primary after:left-0 after:bottom-0 after:scale-x-0 hover:after:scale-x-100 after:transition-transform">Home</Link>
-        </li>
-        
-        
-        
-        
-
         <li>
           <Link href="/resume" className="menu-link">Resume</Link>
         </li>
         <li>
-          <Link href="/contact" className="hover:text-primary">Contact</Link>
+          <Link href="/contact" className="menu-link">Contact</Link>
         </li>
       </ul>
+
+
+      {/* THEME */}
+            <label className="flex cursor-pointer gap-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round">
+          <circle cx="12" cy="12" r="5" />
+          <path
+            d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+        </svg>
+        <input type="checkbox" value="synthwave" className="toggle theme-controller" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round">
+          <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+        </svg>
+      </label>
     </div>
   );
 };
